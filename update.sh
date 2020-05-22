5605f5788a6d8059c91ecdec2c106fc889974189
acpmasquerade <acpmasquerade@gmail.com>
2020-05-22 18:57:45 +0545

    update the new path location

--- update.sh
+++ update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-for a in $(cat all-domains.txt)
+for a_line in $(cat all-domains.txt | sort -u)
 do
-  echo $a;
-	echo "DISPOSABLE" > "api/${a}.html"
+  echo "- ${a_line}"
+  domain=$(echo "${a_line}" | sed 's/^\w+//g' | sed 's/\w$//g' | tr '[:upper:]' '[:lower:]')  
+  first_letter=$(echo "${domain}" | cut -c 1 )
+  echo "-- -- ${first_letter}"
+  echo "-- -- ${domain}"
+  if [ ! -d "api/${first_letter}" ]; then
+    mkdir  "api/${first_letter}/"
+  fi
+  echo "DISPOSABLE" > "api/${first_letter}/${domain}.html"
 done
